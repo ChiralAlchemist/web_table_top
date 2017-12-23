e7f45b957f0dac79b7d608f3f496e6e9643c7770
Joseph Ashby <jashby473@gmail.com>
2017-12-23 12:57:08 -0800

    Updates

--- api/routes.js
+++
@@ -1,9 +0,0 @@
-const mongodbSetup = require('./mongodb')
-function registerRoutes (app) {
-  mongodbSetup.makeUniqueFields();
-  app.post('/api/users',mongodbSetup.postUser)
-  app.get('/api/users', mongodbSetup.getUsers)
-  app.get('/api/login', mongodbSetup.login)
-}
-
-module.exports = registerRoutes;
--- api/schema/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const userShema = mongoose.Schema({
-  name: String,
-  password: String
-})
-
-const User = mongoose.model('User', userShema);
-
-module.exports = User;
