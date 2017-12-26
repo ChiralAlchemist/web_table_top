d77b4cf82ba2c74085218485c095f1c2844c487f
Joseph Ashby <jashby473@gmail.com>
2017-12-26 12:25:47 -0800

    housekeeping

--- client/src/App.js
+++ client/src/App.js
@@ -23,35 +23,12 @@ class App extends Component {
         </ul>
         <Route path="/signup" component={Signup}/>
         <Route path="/login" component={Signup}/>
-      <Route path="/protected/:user" component={TableTop} />
+        <Route path="/protected/:user" component={TableTop} />
         </div>
       </Router>
     );
   }
 }
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  }
-}
 
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//   <Route {...rest} render={props => (
-//     fakeAuth.isAuthenticated ? (
-//       <Component {...props}/>
-//     ) : (
-//       <Redirect to={{
-//         pathname: '/login',
-//         state: { from: props.location }
-//       }}/>
-//     )
-//   )}/>
-// )
 
 export default App;
